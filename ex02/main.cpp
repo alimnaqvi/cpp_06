ee9de7e9019f6840a358402319b56e54637ace3b
Ali Mohsin Naqvi <alimohsinn@hotmail.com>
2025-05-21 16:11:33 +0200

    void dynamic_cast result to silence unused result warning

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -57,7 +57,7 @@ void identify( Base& p )
 {
     try
     {
-        dynamic_cast<A&>( p );
+        (void)dynamic_cast<A&>( p );
         std::cout << "Reference identified: A" << '\n';
         return;
     }
@@ -68,7 +68,7 @@ void identify( Base& p )
 
     try
     {
-        dynamic_cast<B&>( p );
+        (void)dynamic_cast<B&>( p );
         std::cout << "Reference identified: B" << '\n';
         return;
     }
@@ -79,7 +79,7 @@ void identify( Base& p )
 
     try
     {
-        dynamic_cast<C&>( p );
+        (void)dynamic_cast<C&>( p );
         std::cout << "Reference identified: C" << '\n';
         return;
     }
